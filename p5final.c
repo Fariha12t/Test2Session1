d7b090f2cebbad83824b47894e11a961635158cd
Fariha12t <96039101+Fariha12t@users.noreply.github.com>
2022-04-06 15:53:13 +0000

    done

--- p5final.c
+++ p5final.c
@@ -1,46 +1,47 @@
 #include<stdio.h>
-int input_array_size()
+#include<math.h>
+int input()
 {
   int n;
-  printf("enter range:\n");
+  printf("enter the value of n\n");
   scanf("%d",&n);
   return n;
 }
 void init_array(int n,int a[n])
 {
-  for(int i=1;i<=n;i++)
-    {
-      a[i]=i+1;
-    }
+  for(int i=0;i<n;i++)
+  {
+    a[i]=i+1;
+  }
+  a[0]=0;
 }
-void erotosthenes_sieve(int n,int a[n])
+void erotosthenes_sieve(int n, int a[n])
 {
-  for(int i=1;i<n;i++)
+  for(int i=0;i<=sqrt(n);i++)
+  {
+    if(a[i]!=0)
     {
-      if(a[i]%2==0 || a[i]%3==0 || a[i]%5==0 || a[i]%7==0)
-        {
-          a[i]=0;
-        }
+     for(int j=a[i]+a[i];j<=n;j=j+a[i])
+     {
+       a[j-1]=0;
+     }
+    } 
+  }
+}
+void out_put(int n,int a[n])
+{
+  for(int i=0;i<=n;i++)
+  {
+    if(a[i]!=0)
+    {
+      printf("%d\t",a[i]);
     }
   }
-  void out_put(int n,int a[n])
-  {
-    int i;
-    printf("2,3,5,7,");
-    for(i=1;i<n;i++)
-      {  
-         if(a[i]!=0)
-           {
-              printf("%d,",a[i]);
-           }
-      }
-    printf("\n");
-  } 
+}
 int main()
 {
-  int n,c;
-  n=input_array_size();
-  int a[n];
+  int n,a[n];
+  n=input();
   init_array(n,a);
   erotosthenes_sieve(n,a);
   out_put(n,a);
